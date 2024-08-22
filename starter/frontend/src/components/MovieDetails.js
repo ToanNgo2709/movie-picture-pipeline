68598fd8d33c2c26d9ea22bf924526fd2f7da83d
Toan Ngo <ngotoanlibra@gmail.com>
2024-08-22 19:48:43 +0700

    update lint

--- starter/frontend/src/components/MovieDetails.js
+++ starter/frontend/src/components/MovieDetails.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 
 function MovieDetail({ movie }) {
   const [details, setDetails] = useState(null);
-  
   const API_URL = 'http://aab1999ee8a5e419db6ed7c8d400729e-338363811.us-east-1.elb.amazonaws.com';
 
   useEffect(() => {
